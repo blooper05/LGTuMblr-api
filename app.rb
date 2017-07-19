a7ddc033d94b8b16f191e4bbc28e775e5a031163
blooper05 <legend.of.blooper@gmail.com>
2017-07-19 22:32:11 +0900

    :tada: Initial implementation of GET /images API

--- app.rb
+++ app.rb
@@ -1,7 +1,48 @@
 # frozen_string_literal: true
 
-require 'sinatra'
+require 'bundler'
+Bundler.require
 
-get '/' do
-  'Hello World!'
+URL     = 'https://api.tumblr.com/v2/tagged'
+API_KEY = ENV['API_KEY']
+TAGS    = ENV['TAGS'].split(',')
+LIMIT   = 10
+PERIOD  = 6 * 30 * 24 * 60 * 60
+WIDTH   = 500
+
+get '/images' do
+  json = []
+
+  while json.size < LIMIT
+    json += parse(api_response)
+    json.uniq! { |url| url.split('/')[3] }
+  end
+
+  Oj.dump(json)
+end
+
+helpers do
+  def randomized_timestamp
+    now  = Time.now
+    from = now - PERIOD
+    rand(from..now).to_i
+  end
+
+  def api_response
+    params = { api_key: API_KEY,
+               tag:     TAGS.sample,
+               before:  randomized_timestamp }
+    HTTP.get(URL, params: params)
+  end
+
+  def parse(response)
+    Oj.load(response.body.to_s)['response'].map do |res|
+      next unless res['type'] == 'photo'
+      res['photos'].map do |photo|
+        photo['alt_sizes'].map do |size|
+          size['url'] if size['width'] == WIDTH
+        end
+      end
+    end.flatten.compact
+  end
 end
